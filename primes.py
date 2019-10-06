5051293c40db62b7e2fd021d12a660084332f787
wnuke <willem@leit.so>
2019-10-06 22:19:17 +0200

    made the output of primes.py nicer and more detailed

--- primes.py
+++ primes.py
@@ -1,13 +1,19 @@
-y = [2] #this array will contain the prime numbers that are found
+y = [2] # this array will contain the prime numbers that are found
+x = 100 # will check for primes up to this number
+p = 1 # number of primes found
 
-for i in range(3, 10000):
+print("Looking for primes from 0 to " + str(x) + ".")
+print("2 is a prime.")
+
+for i in range(3, x):
     isaprime = 1
     for u in range(0, len(y)):
         if (i % y[u]) == 0:
             isaprime = 0
             break
     if isaprime == 1:
-        print(i)
+        print(str(i) + " is a prime.")
         y.append(i)
+        p = p + 1
 
-print(y)
+print("Found " + str(p) + " prime(s) from 0 to " + str(x) + ".")
